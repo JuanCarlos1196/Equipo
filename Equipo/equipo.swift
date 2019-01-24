eb334a3292b86e7523566e34f8332dd0144dc7bf
Leidy palomino <leidyyajaira@gmail.com>
2019-01-24 08:22:09 -0600

    project init

---
+++ Equipo/equipo.swift
@@ -0,0 +1,13 @@
+//
+//  equipo.swift
+//  Equipo
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 1/24/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class equipo: NSObject {
+
+}
