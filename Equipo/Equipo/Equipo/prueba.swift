7ec7aaded86e9c25e8e33b85836b3b78c2093764
JuanCarlos1196 <juan1196mtz@gmail.com>
2019-01-24 08:25:57 -0600

    project prueba

---
+++ Equipo/Equipo/Equipo/prueba.swift
@@ -0,0 +1,35 @@
+//
+//  prueba.swift
+//  Equipo
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 24/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class prueba: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
